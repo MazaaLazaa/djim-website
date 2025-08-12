3fece3b947d63c00a3e9b47ab5e196d55b3ae3bf
MazaaLazaa <146481617+MazaaLazaa@users.noreply.github.com>
2025-08-12 03:11:57 +0000

    ffcde

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -1,40 +1,38 @@
-import { ImageResponse } from 'next/server'
+import { ImageResponse } from 'next/og'
 
 export const runtime = 'edge'
 
 export async function GET(request: Request) {
-  try {
-    const { searchParams } = new URL(request.url)
-    const title = searchParams.get('title') || 'Next.js Portfolio Starter'
+  const { searchParams } = new URL(request.url)
+  const title = searchParams.get('title') ?? 'Next.js Portfolio Starter'
 
-    return new ImageResponse(
-      (
-        <div
+  return ImageResponse(
+    (
+      <div
+        style={{
+          backgroundColor: 'black',
+          height: '100%',
+          width: '100%',
+          display: 'flex',
+          textAlign: 'center',
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexDirection: 'column',
+        }}
+      >
+        <h1
           style={{
-            fontSize: 40,
-            color: 'black',
-            background: 'white',
-            width: '100%',
-            height: '100%',
-            padding: '50px 200px',
-            textAlign: 'center',
-            justifyContent: 'center',
-            alignItems: 'center',
-            display: 'flex',
+            fontSize: 60,
+            letterSpacing: '-0.025em',
+            color: 'white',
+            margin: '0 30px',
+            whiteSpace: 'pre-wrap',
           }}
         >
           {title}
-        </div>
-      ),
-      {
-        width: 1200,
-        height: 630,
-      }
-    )
-  } catch (e: any) {
-    console.log(`${e.message}`)
-    return new Response(`Failed to generate the image`, {
-      status: 500,
-    })
-  }
+        </h1>
+      </div>
+    ),
+    { width: 1200, height: 630 }
+  )
 }
